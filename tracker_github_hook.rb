d30027575906961da33190bee633ea8827a06b4c
Christopher Bailey <chris@cobaltedge.com>
2009-12-03 13:31:42 -0800

    Add support for individual Pivotal Tracker API keys, so you can have the Tracker comment made by the samer person who did the GitHub commit.

--- tracker_github_hook.rb
+++ tracker_github_hook.rb
@@ -25,8 +25,17 @@ configure do
     
     PROJECTS = Hash.new
     config.each do |project|
-      raise "required configuration settings not found" unless project[1]['tracker_api_token'] && project[1]['tracker_project_id']    
-      PROJECTS[project[1]['github_url']] = { :api_token => project[1]['tracker_api_token'], :project_id => project[1]['tracker_project_id'], :ref => project[1]['ref'] }
+      raise "required configuration settings not found" unless project[1]['tracker_api_token'] && project[1]['tracker_project_id'] 
+      api_tokens = Hash.new
+      if project[1]['user_api_tokens']
+        project[1]['user_api_tokens'].each_value do |user_info|
+          api_tokens[user_info['email'].downcase] = user_info['tracker_api_token']
+        end
+      end
+      PROJECTS[project[1]['github_url']] = { :api_token => project[1]['tracker_api_token'], 
+                                             :project_id => project[1]['tracker_project_id'], 
+                                             :ref => project[1]['ref'],
+                                             :user_api_tokens => api_tokens }
     end
   rescue => e
     puts "Failed to startup: #{e.message}"
@@ -57,6 +66,9 @@ helpers do
   def process_commit(tracker_info, commit)
     # get commit message
     message = commit['message']
+    
+    # get API token for the user who made the commit, if possible
+    api_token = api_token_for_user(tracker_info, commit['author']['email'])
 
     # see if there is a Tracker story trigger, and if so, get story ID
     message.scan(/\[Story(\d+)([^\]]*)\]/) do |tracker_trigger|
@@ -66,7 +78,7 @@ helpers do
       # post comment to the story
       RestClient.post(create_api_url(tracker_info[:project_id], story_id, '/notes'),
                       "<note><text>(from [#{commit['id']}]) #{message}</text></note>", 
-                      tracker_api_headers(tracker_info[:api_token]))
+                      tracker_api_headers(api_token))
     
       # See if we have a state change
       state = tracker_trigger[1].match(/.*state:(\s?\w+).*/)
@@ -75,9 +87,13 @@ helpers do
 
         RestClient.put(create_api_url(tracker_info[:project_id], story_id), 
                        "<story><current_state>#{state}</current_state></story>", 
-                       tracker_api_headers(tracker_info[:api_token]))
+                       tracker_api_headers(api_token))
       end     
     end
+  end
+  
+  def api_token_for_user(tracker_info, email)
+    tracker_info[:user_api_tokens][email.downcase] || tracker_info[:api_token]
   end
 
   def create_api_url(project_id, story_id, extra_path_elemets='')
